5f7d523f55fe6ffdf6a6448df1af7578e963e3fb
ZAM@1CLICK.LV <ZAM@1CLICK.LV>
2017-04-26 22:52:26 +0300

    API moved to library folder

--- FaceAPI/FaceAPI.dpr
+++ FaceAPI/FaceAPI.dpr
@@ -3,11 +3,11 @@ program FaceAPI;
 uses
   Vcl.Forms,
   ufmMain in 'ufmMain.pas' {Form1},
-  uFaceApi in 'uFaceApi.pas',
   uFunctions.StringHelper in 'Functions\uFunctions.StringHelper.pas',
-  uFaceApi.Servers.Types in 'uFaceApi.Servers.Types.pas',
-  uFaceApi.Content.Types in 'uFaceApi.Content.Types.pas',
-  uFaceApi.Base in 'uFaceApi.Base.pas';
+  uFaceApi.Base in 'Library\uFaceApi.Base.pas',
+  uFaceApi.Content.Types in 'Library\uFaceApi.Content.Types.pas',
+  uFaceApi in 'Library\uFaceApi.pas',
+  uFaceApi.Servers.Types in 'Library\uFaceApi.Servers.Types.pas';
 
 {$R *.res}
 
